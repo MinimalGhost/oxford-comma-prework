1cb7728e0c19fcdd8451b20e5bacdbe8517b459e
Eric Kollegger <eric.kollegger@gmail.com>
2017-11-09 15:47:34 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,7 @@ def oxford_comma(array)
     return array.join('')
   elsif array.length == 2
     array.join(' and ')
-  else
+  elseif array.length == 3
     array[2] = "and #{array[2]}"
     array.join(', ')
   end
